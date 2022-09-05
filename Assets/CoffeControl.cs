f912217ca28f78762b25e7ba82d149f6bc5853bb
Juan Andres Osorio <jaot12345@gmail.com>
2022-09-05 12:39:53 -0500

    Correccion colliders y codigo de la cafetera

--- Assets/CoffeControl.cs
+++ Assets/CoffeControl.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class CoffeControl : MonoBehaviour
 {
+    [SerializeField] private ParticleSystem _particleSystem; 
+
+    private bool _grab;
+    private bool _buttonPress;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,30 @@ public class CoffeControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (_grab && _buttonPress)
+        {
+            _particleSystem.Play();
+        }
+        else
+        {
+            _particleSystem.Stop();
+        }
+    }
+
+    public void OnGrab()
+    {
+        _grab = true;
+    }
+    public void Put()
+    {
+        _grab = false;
+    }
+    public void ButtonPress()
+    {
+        _buttonPress = true;
+    }
+    public void ButtonUp()
+    {
+        _buttonPress = false;
     }
 }
